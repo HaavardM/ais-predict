c4c389fb6b3891b79439d2edcbad047db12bf3f5
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2021-04-26 14:51:13 +0200

    Update dyngp_kalman

--- ais_predict/trajpred/dyngp.py
+++ ais_predict/trajpred/dyngp.py
@@ -10,6 +10,7 @@ def samples_from_lag_n_df(df: gpd.GeoDataFrame, n: int):
 
     X = np.empty((len(df.index), n, 2))
     Y = np.empty((len(df.index), n, 2))
+    T = np.empty((len(df.index), n))
     for i in range(n):
         
         l1 = f"_{i}"
@@ -26,7 +27,8 @@ def samples_from_lag_n_df(df: gpd.GeoDataFrame, n: int):
 
         X[:, i, :] = np.vstack([p1_x, p1_y]).T
         Y[:, i, :] = np.vstack([p2_x - p1_x, p2_y - p1_y]).T / dt.reshape((-1, 1))
-    return X.reshape((-1, 2)), Y.reshape((-1, 2))
+        T[:, i] = (df["timestamp" + l1] - df["timestamp"]).dt.seconds.to_numpy()
+    return X.reshape((-1, 2)), Y.reshape((-1, 2)), T.flatten()
 
 @njit(nogil=True, parallel=True)
 def kernel(X1: np.ndarray, X2: np.ndarray, sigma: float, lengthscale: float) -> np.ndarray:
@@ -42,7 +44,7 @@ def dkernel(X1: np.ndarray, X2:np.ndarray, sigma: float, lengthscale: float) -> 
     K = np.empty((X1.shape[0], X2.shape[0], X1.shape[-1]))
     for d in range(K.shape[-1]):
         K[:, :, d] = (-(X1[:, d] - X2[:, d]) * kernel(X1, X2, sigma, lengthscale).T)
-    return K / lengthscale
+    return K / (lengthscale**2)
 
 @njit(nogil=True)
 def solve_lower_triangular(L: np.ndarray, b: np.ndarray, lower: bool = True, replace_b: bool = False) -> np.ndarray:
@@ -66,7 +68,7 @@ def init(train_x: np.ndarray, train_delta_y: np.ndarray,  sigma: float, lengthsc
         sigma=sigma, 
         lengthscale=lengthscale
     )
-    L = np.linalg.cholesky(k + noise * np.eye(k.shape[0]))
+    L = np.linalg.cholesky(k + (noise**2) * np.eye(k.shape[0]))
     alpha = solve_lower_triangular(L, train_delta_y)
     alpha = solve_lower_triangular(L.T, alpha, lower=False)
     return L, alpha, train_x
@@ -91,10 +93,24 @@ def predict(ctx: tuple, pred_x: np.ndarray, sigma: float, lengthscale: float) ->
     ) - v.T @ v
     return mean, var
 
-def predict_G(ctx: tuple, pred_x: np.ndarray, sigma: float, lengthscale: float) -> np.ndarray:
+@njit(nogil=True)
+def predict_F(ctx: tuple, pred_x: np.ndarray, sigma: float, lengthscale: float) -> np.ndarray:
     _, alpha, train_x = ctx
     dk = dkernel(pred_x, train_x, sigma, lengthscale)
-    return dk.transpose((0, 2, 1)) @ alpha
+    G = np.empty((pred_x.shape[0], pred_x.shape[-1], pred_x.shape[-1]))
+    for t in prange(dk.shape[0]):
+         G[t] = dk[t].T @ alpha
+    return G
+
+#@njit(nogil=True)
+def sample_f(ctx: tuple, pred_x: np.ndarray, sigma: float, lengthscale: float) -> np.ndarray:
+    f, std = predict(
+        ctx=ctx, 
+        pred_x=pred_x, 
+        sigma=sigma, 
+        lengthscale=lengthscale
+    )
+    return np.random.normal(loc=f, scale=std)
 
 #@njit(nogil=True)
 def dyngp_kalman(train_x: np.ndarray, train_delta_y: np.ndarray, init_x: np.ndarray, sigma: float, lengthscale: float, noise: float, steps: int = 1000, dt: float = 1) -> np.ndarray:
@@ -112,6 +128,7 @@ def dyngp_kalman(train_x: np.ndarray, train_delta_y: np.ndarray, init_x: np.ndar
     var = np.empty((N_trajectories, steps, 2, 2))
     var[:, 0] = np.eye(2)
     x[:, 0] = init_x 
+    dt_squared_eye = np.eye(2) * dt ** 2
     for traj in prange(N_trajectories):
         for i in range(1, steps):
             m, v = predict(
@@ -121,14 +138,30 @@ def dyngp_kalman(train_x: np.ndarray, train_delta_y: np.ndarray, init_x: np.ndar
                 lengthscale=lengthscale
             )
             x[traj, i, :] = x[traj, i-1, :] + m * dt
-            G = predict_G(
+            F = predict_F(
                 ctx=ctx, 
                 pred_x=x[traj, i-1, :].reshape((1, -1)),
                 sigma=sigma,
                 lengthscale=lengthscale
-            )[0]
-            var[traj, i] = G @ var[traj, i-1] @ G.T + np.eye(2)*v
+            )[0] * dt
+            var[traj, i] = F.T @ var[traj, i-1] @ F + v * dt_squared_eye
     return x, var
+
+def dyngp_particle(train_x: np.ndarray, train_delta_y: np.ndarray, init_x: np.ndarray, sigma: float, lengthscale: float, noise: float, steps: int = 1000, dt: float = 1, particles: int = 1000) -> np.ndarray:
+    ctx = init(
+        train_x=train_x,
+        train_delta_y=train_delta_y,
+        sigma=sigma,
+        lengthscale=lengthscale,
+        noise=noise
+    )
+    x = np.empty((steps, particles) + (init_x.shape[-1],))
+    x[0, :] = init_x 
+    for step in range(1, steps):
+        for particle in prange(particles):
+            x[step, particle] = x[step-1, particle] + sample_f(ctx, x[step-1, particle].reshape((1, -1)), sigma=sigma, lengthscale=lengthscale)*dt
+    return x
+
 
 import matplotlib.pyplot as plt
 def plot_uncertianty_grid(train_x: np.ndarray, train_delta_y: np.ndarray, sigma: float, lengthscale: float, noise: float, xlim=(-1, 1), ylim=(-1,1), size=(20, 20)):
@@ -155,13 +188,16 @@ def plot_uncertianty_grid(train_x: np.ndarray, train_delta_y: np.ndarray, sigma:
     x1, x2 = xlim
     y1, y2 = ylim
     sx, sy = size
-    pX, pY = np.mgrid[
-        x1:x2:(x2-x1)/sx,
-        y1:y2:(y2-y1)/sy
-    ]
+    dX = x2 - x1
+    dY = y2 - y1
+    #x1 -= dX / (sx+1)
+    x2 += dX / (sx+1)
+    #y1 -= dY / (sy+1)
+    y2 += dY / (sy+1)
+    pX, pY = np.meshgrid(np.linspace(x1, x2, sx+1), np.linspace(y1, y2, sy+1))
     pred_x = np.vstack([pX.ravel(), pY.ravel()]).T
     mean, var = predict(ctx, pred_x, sigma=sigma, lengthscale=lengthscale)
-    std = np.sqrt(var.diagonal()).reshape(size)
+    std = np.sqrt(var.diagonal()).reshape((sx+1, sy+1))
     plt.pcolormesh(pX, pY, std, cmap="Greys", shading="gouraud")
     #plt.quiver(*pred_x.T, *pred_f.T)
     plt.quiver(*train_x.T, *train_delta_y.T, color="red", alpha=0.1)
